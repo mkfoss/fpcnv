322bc4377155a5629df3c8b2d86cf899cb2670f3
Sean Mathews <redmonkeydf+git@gmail.com>
2025-10-18 18:54:50 -0400

    internal header reading funcs completed

--- fp.header.go
+++ fp.header.go
@@ -3,21 +3,29 @@ package fpcnv
 import (
 	"encoding/binary"
 	"slices"
+	"time"
 )
 
 type Header struct {
-	Magic uint8 // file magic byte - what type of dbf
+	Magic         uint8 // file magic byte - what type of dbf
+	LastUpdate    time.Time
+	RecordCount   uint32
+	RecordSize    uint16
+	RecordsOffset uint16
+	HasIndex      bool
+	HasFpt        bool
+	IsDatabase    bool
+	Codepage      Codepage
 }
 
-var mbsupported []byte = []byte{0x30}
+var mbsupported = []byte{0x30}
 
 func readHeader(f FPFiler) (*Header, error) {
 
 	return nil, NewError("not implemented")
 }
 
-func readMagicByte(f FPFiler) (byte, error) {
-
+func readHdrMagicByte(f FPFiler) (byte, error) {
 	var b byte
 	if err := binary.Read(f, binary.LittleEndian, &b); err != nil {
 		return 0, NewError("failed to read magic byte").SetWrapped(err)
@@ -28,4 +36,119 @@ func readMagicByte(f FPFiler) (byte, error) {
 	}
 
 	return b, nil
+}
+
+func readHdrLastUpdate(f FPFiler) (time.Time, error) {
+
+	b := make([]byte, 3)
+	if err := binary.Read(f, binary.LittleEndian, &b); err != nil {
+		return time.Time{}, NewError("failed to read last update time").SetWrapped(err)
+	}
+
+	errformatstr := "invalid %s: %d in last updated date"
+
+	if b[0] > 99 {
+		return time.Time{}, NewErrorf(errformatstr, "year", b[0])
+	}
+	yr := int(b[0]) //year 200 compensation
+	if b[0] >= 70 {
+		yr += 1900
+	} else {
+		yr += 2000
+	}
+
+	if b[1] == 0 || b[1] > 12 {
+		return time.Time{}, NewErrorf(errformatstr, "month", b[1])
+	}
+
+	daysinmnth := time.Date(yr, time.Month(b[1]+1), 0, 0, 0, 0, 0, time.Local).Day()
+	if b[2] == 0 || int(b[2]) > daysinmnth {
+		return time.Time{}, NewErrorf(errformatstr, "day", b[2])
+	}
+
+	return time.Date(yr, time.Month(b[1]), int(b[2]), 0, 0, 0, 0, time.Local), nil //time for dbf files always local
+}
+
+func readHdrNumRecords(f FPFiler) (uint32, error) {
+
+	var b uint32
+	err := binary.Read(f, binary.LittleEndian, &b)
+	if err != nil {
+		return 0, NewError("failed to read num records").SetWrapped(err)
+	}
+	if b == 0 {
+		return 0, NewErrorf("invalid record count, dbf files require at least one record").SetWrapped(nil)
+	}
+	return b, nil
+}
+
+func readUint16(f FPFiler) (uint16, error) {
+
+	var b uint16
+	err := binary.Read(f, binary.LittleEndian, &b)
+	if err != nil {
+		return 0, NewError("failed to read uint16").SetWrapped(err)
+	}
+
+	return b, nil
+}
+
+func readHdrRecordOffset(f FPFiler) (uint16, error) {
+
+	b, err := readUint16(f)
+	if err != nil {
+		return 0, NewError("failed to read offset").SetWrapped(err)
+	}
+
+	if b < 33 {
+		return 0, NewErrorf("invalid record offset").SetWrapped(nil)
+	}
+
+	return b, nil
+}
+
+func readHdrRecordSize(f FPFiler) (uint16, error) {
+
+	b, err := readUint16(f)
+	if err != nil {
+		return 0, NewError("failed to read record size").SetWrapped(err)
+	}
+
+	if b < 2 {
+		return 0, NewErrorf("invalid record size").SetWrapped(nil)
+	}
+
+	return b, nil
+}
+
+func readHdrTableFlags(f FPFiler) (bool, bool, error) {
+
+	var b byte
+	if err := binary.Read(f, binary.LittleEndian, &b); err != nil {
+		return false, false, NewError("failed to read flags").SetWrapped(err)
+	}
+
+	if b&0x04 == 0x04 {
+		return false, false, NewErrorf("fp databases not supported").SetWrapped(nil)
+	}
+
+	if b > 7 {
+		return false, false, NewErrorf("invalid table flags").SetWrapped(nil)
+	}
+
+	return b&0x01 == 0x01, b&0x02 == 0x02, nil
+}
+
+func readHdrCodepage(f FPFiler) (Codepage, error) {
+
+	var b uint8
+	if err := binary.Read(f, binary.LittleEndian, &b); err != nil {
+		return 0, NewError("failed to read codepage").SetWrapped(err)
+	}
+
+	if !slices.Contains(supportedCodepages, Codepage(b)) {
+		return 0, NewErrorf("unsupported codepage 0x%.2x", b)
+	}
+
+	return Codepage(b), nil
 }
--- fp.header_test.go
+++ fp.header_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -29,7 +30,7 @@ func Test_HeaderReadMagicbyte(t *testing.T) {
 			fl.Data = make([]byte, 32)
 			fl.Data[0] = tc.input
 
-			mb, err := readMagicByte(fl)
+			mb, err := readHdrMagicByte(fl)
 			if tc.error != "" {
 				if err == nil {
 					t.Fatal("expected error, got nil")
@@ -45,7 +46,7 @@ func Test_HeaderReadMagicbyte(t *testing.T) {
 	t.Run("Magic Byte - No data", func(t *testing.T) {
 		fl := NewMockFiler()
 
-		mb, err := readMagicByte(fl)
+		mb, err := readHdrMagicByte(fl)
 		assert.Error(t, err)
 		assert.ErrorContains(t, err, "failed to read magic byte")
 
@@ -58,4 +59,279 @@ func Test_HeaderReadMagicbyte(t *testing.T) {
 
 		assert.Empty(t, mb)
 	})
+}
+
+func Test_HeaderReadLastModified(t *testing.T) {
+
+	type testtype struct {
+		id       int
+		input    []byte
+		expected string
+		error    string
+	}
+
+	cases := []testtype{
+		{1, []byte{25, 10, 10}, "2025-10-10", ""},
+		{2, []byte{32, 2, 5}, "2032-02-05", ""},
+		{3, []byte{89, 10, 11}, "1989-10-11", ""},
+		{4, []byte{100, 10, 11}, "0001-01-01", "invalid year: 100 in last updated date"},
+		{5, []byte{23, 13, 11}, "0001-01-01", "invalid month: 13 in last updated date"},
+		{6, []byte{25, 10, 32}, "0001-01-01", "invalid day: 32 in last updated date"},
+		{7, []byte{20, 2, 29}, "2020-02-29", ""},
+		{8, []byte{20, 2, 30}, "0001-01-01", "invalid day: 30 in last updated date"},
+		{9, []byte{19, 2, 29}, "0001-01-01", "invalid day: 29 in last updated date"},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("Read LastUpdated #%.2d", tc.id), func(t *testing.T) {
+
+			expectedtm, err := time.ParseInLocation("2006-01-02", tc.expected, time.Local)
+			if err != nil {
+				t.Fatal(err, "should not fail")
+			}
+			fl := NewMockFiler()
+			fl.Data = make([]byte, 32)
+			fl.Data[1] = tc.input[0]
+			fl.Data[2] = tc.input[1]
+			fl.Data[3] = tc.input[2]
+			fl.Pos = 1
+
+			gottm, err := readHdrLastUpdate(fl)
+			if tc.error != "" {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				assert.ErrorContains(t, err, tc.error)
+				assert.Equal(t, gottm, time.Time{})
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, expectedtm, gottm)
+			}
+		})
+	}
+
+}
+
+func Test_HeaderReadNumRecords(t *testing.T) {
+	type testType struct {
+		id       int
+		input    []byte
+		expected uint32
+		error    string
+	}
+
+	cases := []testType{
+		{1, []byte{0x0c, 0x10, 0x00, 0x00}, uint32(4108), ""},
+		{2, []byte{0x00, 0x00, 0x00, 0x00}, uint32(0), "invalid record count, dbf files require at least one record"},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("Read NumRecords #%.2d", tc.id), func(t *testing.T) {
+			fl := NewMockFiler()
+			fl.Data = make([]byte, 32)
+			fl.Data[4] = tc.input[0]
+			fl.Data[5] = tc.input[1]
+			fl.Data[6] = tc.input[2]
+			fl.Data[7] = tc.input[3]
+			fl.Pos = 4
+
+			gottm, err := readHdrNumRecords(fl)
+			if tc.error != "" {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				assert.ErrorContains(t, err, tc.error)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, gottm)
+			}
+		})
+	}
+}
+
+func Test_ReadUint16(t *testing.T) {
+	type testType struct {
+		id       int
+		input    []byte
+		expected uint16
+		error    string
+	}
+
+	cases := []testType{
+		{1, []byte{0x0c, 0x10}, uint16(4108), ""},
+		{2, []byte{0x00, 0x00}, uint16(0), ""},
+		{3, []byte{0x88, 0x04}, uint16(1160), ""},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("Read Uint16 #%.2d", tc.id), func(t *testing.T) {
+			fl := NewMockFiler()
+			fl.Data = make([]byte, 32)
+			fl.Data[8] = tc.input[0]
+			fl.Data[9] = tc.input[1]
+			fl.Pos = 8
+
+			gotint, err := readUint16(fl)
+			if tc.error != "" {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				assert.ErrorContains(t, err, tc.error)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, gotint)
+			}
+		})
+	}
+}
+
+func Test_ReadRecOffset(t *testing.T) {
+
+	type testType struct {
+		id       int
+		input    []byte
+		expected uint16
+		error    string
+	}
+
+	cases := []testType{
+		{1, []byte{0x0c, 0x10}, uint16(4108), ""},
+		{2, []byte{0x00, 0x00}, uint16(0), "invalid record offset"},
+		{3, []byte{0x01, 0x00}, uint16(0), "invalid record offset"},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("Read RecOffset #%.2d", tc.id), func(t *testing.T) {
+			fl := NewMockFiler()
+			fl.Data = make([]byte, 32)
+			fl.Data[10] = tc.input[0]
+			fl.Data[11] = tc.input[1]
+			fl.Pos = 10
+
+			ro, err := readHdrRecordOffset(fl)
+			if tc.error != "" {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				assert.ErrorContains(t, err, tc.error)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, ro)
+			}
+		})
+	}
+}
+
+func Test_ReadRecordSize(t *testing.T) {
+	type testType struct {
+		id       int
+		input    []byte
+		expected uint16
+		error    string
+	}
+	cases := []testType{
+		{1, []byte{0x0c, 0x10}, uint16(4108), ""},
+		{2, []byte{0x00, 0x00}, uint16(0), "invalid record size"},
+		{3, []byte{0x01, 0x00}, uint16(0), "invalid record size"},
+		{3, []byte{0x02, 0x00}, uint16(2), ""},
+	}
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("Read RecSize #%.2d", tc.id), func(t *testing.T) {
+			fl := NewMockFiler()
+			fl.Data = make([]byte, 32)
+			fl.Data[10] = tc.input[0]
+			fl.Data[11] = tc.input[1]
+			fl.Pos = 10
+
+			b, err := readHdrRecordSize(fl)
+			if tc.error != "" {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				assert.ErrorContains(t, err, tc.error)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, uint16(b))
+			}
+		})
+	}
+}
+
+func Test_ReadTableFlags(t *testing.T) {
+	type testType struct {
+		id     int
+		input  byte
+		hasidx bool
+		hasfpt bool
+		error  string
+	}
+
+	cases := []testType{
+		{1, 0, false, false, ""},
+		{2, 1, true, false, ""},
+		{3, 2, false, true, ""},
+		{4, 3, true, true, ""},
+		{5, 4, false, false, "fp databases not supported"},
+		{6, 5, true, false, "fp databases not supported"},
+		{7, 6, false, true, "fp databases not supported"},
+		{8, 7, true, true, "fp databases not supported"},
+		{9, 8, false, false, "invalid table flags"},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("Read TableFlags #%.2d", tc.id), func(t *testing.T) {
+			fl := NewMockFiler()
+			fl.Data = make([]byte, 32)
+			fl.Data[28] = tc.input
+			fl.Pos = 28
+
+			hasidx, hsfpt, err := readHdrTableFlags(fl)
+			if tc.error != "" {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				assert.ErrorContains(t, err, tc.error)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.hasidx, hasidx)
+				assert.Equal(t, tc.hasfpt, hsfpt)
+			}
+		})
+	}
+}
+
+func Test_ReadCodepage(t *testing.T) {
+	type testType struct {
+		id       int
+		input    byte
+		expected Codepage
+		error    string
+	}
+
+	cases := []testType{
+		{1, 0x03, Codepage(0x03), ""},
+		{2, 0x00, Codepage(0x00), "unsupported codepage 0x00"},
+		{3, 0xff, Codepage(0x00), "unsupported codepage 0xff"},
+		{3, 0x4b, Codepage(0x00), "unsupported codepage 0x4b"},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("Read Codepage #%.2d", tc.id), func(t *testing.T) {
+			fl := NewMockFiler()
+			fl.Data = make([]byte, 32)
+			fl.Data[29] = tc.input
+			fl.Pos = 29
+
+			cp, err := readHdrCodepage(fl)
+			if tc.error != "" {
+				if err == nil {
+					t.Fatal("expected error, got nil")
+				}
+				assert.ErrorContains(t, err, tc.error)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expected, cp)
+			}
+		})
+	}
 }
